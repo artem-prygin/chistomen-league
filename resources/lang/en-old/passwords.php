17e5cedd8b19e7f0ccaecb5d4e0cd8ea1ba5c523
Artem Prygin <a.prygin@wellyes.ru>
2020-05-07 22:28:42 +0300

    locale hard fix

--- resources/lang/en-old/passwords.php
+++ resources/lang/en-old/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'reset' => 'Пароль успешно изменен',
-    'sent' => 'Письмо с ссылкой для восстановления пароля отправлено',
-    'throttled' => 'Повторите попытку позже',
-    'token' => 'Неверный токен для восстановления пароля',
-    'user' => "Участника с такой почтой не зарегистрировано",
+    'reset' => 'Your password has been reset!',
+    'sent' => 'We have emailed your password reset link!',
+    'throttled' => 'Please wait before retrying.',
+    'token' => 'This password reset token is invalid.',
+    'user' => "We can't find a user with that email address.",
 
 ];
